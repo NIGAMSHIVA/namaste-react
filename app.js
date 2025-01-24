cf85a3a25b4f02f1193b8c643ff6f3d96e1dd218
NIGAMSHIVA <210105056@hbtu.ac.in>
2025-01-24 12:53:05 +0530

    second commit

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },[  React.createElement("div", { id: "child" }, [
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I Love Naruto"),
     React.createElement("h2", {}, "I also Love Light Yagami"),
   ]),
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I Love Naruto"),
     React.createElement("h2", {}, "I also Love Light Yagami"),
-  ])
+  ]),
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
