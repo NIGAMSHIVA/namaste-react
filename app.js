f7880a1506722a211ec4178b2c4c0406a8a16e41
NIGAMSHIVA <210105056@hbtu.ac.in>
2025-01-28 17:51:43 +0530

    Learn Rendering Components and Creating them

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ import ReactDOM from "react-dom/client";
 import { jsx } from "react/jsx-runtime";
 
 const jsxHeading = (
-  <h1 className="heading" tabIndex={5}>
+  <h1 className="head" tabIndex="5">
     Namaste React Using JSX
   </h1>
 );
 
-console.log(jsxHeading);
+// Everything in the React are Components,
+
+const HeadingComponent = () => {
+ return (
+    <div id="container">
+     <h1 className="heading">Shiva Nigam is My name</h1>
+    </div>
+  );
+  
+}
+
+console.log(HeadingComponent);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+
+root.render(<HeadingComponent />);
+
