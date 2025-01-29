02decdc05c1bfdf5a2adfdd7962ee33cecf2fe4e
NIGAMSHIVA <210105056@hbtu.ac.in>
2025-01-29 12:03:44 +0530

    Learned Component Composition

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { jsx } from "react/jsx-runtime";
 
-const jsxHeading = (
+const Heading = () => (
   <h1 className="head" tabIndex="5">
-    Namaste React Using JSX
+    Namaste React Using JSX 🚀
   </h1>
 );
 
@@ -12,15 +12,13 @@ const jsxHeading = (
 
 const HeadingComponent = () => {
  return (
-    <div id="container">
-     <h1 className="heading">Shiva Nigam is My name</h1>
+   <div id="container">
+     <Heading />
+     <h1 className="heading">Namaste react functional Component</h1>
     </div>
   );
   
 }
-
-console.log(HeadingComponent);
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
