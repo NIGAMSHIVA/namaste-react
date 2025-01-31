fdedf10d1d0912e53752707745f7f82cbc4372c2
NIGAMSHIVA <210105056@hbtu.ac.in>
2025-01-31 09:36:55 +0530

    Created Header Layout

--- app.js
+++ app.js
@@ -1,27 +1,77 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { jsx } from "react/jsx-runtime";
 
-const Heading = () => (
-  <h1 className="head" tabIndex="5">
-    Namaste React Using JSX 🚀
-  </h1>
-);
+/*
+Header
+ -Logo
+ -nav-items
 
-// Everything in the React are Components,
+Body
+ -Search
+ -Restaurant-Container
+  -Restaurant Cart
+Footer
+ -Copyright
+ -links
+ -address
+ -Contacts
 
-const HeadingComponent = () => {
- return (
-   <div id="container">
-     <Heading />
-     <h1 className="heading">Namaste react functional Component</h1>
+
+*/
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/cheese-burger-with-lettuce-and-cheese-2697ld.png?size=2"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
-  
+};
+
+const RestaurantCard = () => {
+
+  return (
+    <div className="res-card">
+      <h3>Meghana Foods</h3>
+    </div>
+
+
+  );
 }
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search-bar">Search</div>
+
+      <div className="res-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-root.render(<HeadingComponent />);
-
+root.render(<AppLayout />);
