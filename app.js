87213a075361885e6e6596bba3b946ca755cc363
NIGAMSHIVA <210105056@hbtu.ac.in>
2025-02-01 09:58:13 +0530

    Restaurant card

--- app.js
+++ app.js
@@ -40,21 +40,30 @@ const Header = () => {
   );
 };
 
+const stylecard = {
+  backgroundColor: "#f0f0f0",
+};
+
 const RestaurantCard = () => {
-
   return (
-    <div className="res-card">
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        alt="res-logo"
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/cvo1djhbwrgfqd64k0tl"
+      />
       <h3>Meghana Foods</h3>
+      <h4>Biryani,North Indian,Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
     </div>
-
-
   );
-}
+};
 
 const Body = () => {
   return (
     <div className="body">
-      <div className="search-bar">Search</div>
+      <div className="search">Search</div>
 
       <div className="res-container">
         <RestaurantCard />
